79b898a8bda5efb3a6667dc893c254a56df77ec8
Dziga <dzigaczaja@gmail.com>
2024-12-23 18:02:33 +0100

    functional

--- ast.ml
+++ ast.ml
@@ -14,28 +14,23 @@ let rec print_sep_spec = function
   | [x] -> print_string "|-"
   | x :: q -> print_string x; print_sep_spec q
     
-let rec aff_aux l a =
-  
-  print_sep_spec l;
+let rec aff_aux a =
   match a with
-  | Group(a) -> 
-    print_string "Lpar";
-    aff_aux l a;
-    print_string "RPar";
+  | Group(a1) -> 
+    print_string "(";
+    aff_aux a1;
+    print_string ")";
   | Plus(a1, a2) ->
-    print_string "Plus(";
-    aff_aux (l @ [" "]) a1;
-    print_sep (l @ ["|"]);
-    aff_aux (l @ [")"]) a2
+    aff_aux a1;
+    print_string "+";
+    aff_aux a2;
   | Times(a1, a2) ->
-    print_string "Fois";
-    print_sep (l @ ["|"]);
-    aff_aux (l @ ["| "]) a1;
-    print_sep (l @ ["|"]);
-    aff_aux (l @ ["  "]) a2
+    aff_aux a1;
+    print_string "*";
+    aff_aux a2;
   | Cst i -> Printf.printf "Cte(%i)" i
 
   | Id s -> Printf.printf "Id(%s)" s
 
-let affiche = aff_aux []
+let affiche = aff_aux
   
